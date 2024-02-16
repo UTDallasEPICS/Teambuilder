0e244b46a2509492495b3112028a4f6ac38b6f43
jbthelegend <jbthelegend15@gmail.com>
2024-02-15 18:13:18 -0600

    2/15, node.js tutorial hello teambuilder

---
+++ frontend/src/Message.tsx
@@ -0,0 +1,11 @@
+  
+  function Message () {
+    //JSX: java script xml. html is converted to javascript
+    const name = 'Team Builder';
+    return <h1>Hello {name}</h1>;
+
+
+  }
+
+
+  export default Message;
