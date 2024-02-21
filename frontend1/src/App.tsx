5ad882638e3cca087d0f94c5b642fe4416c3b758
jbthelegend <jbthelegend15@gmail.com>
2024-02-20 23:10:33 -0600

    lol

--- frontend1/src/App.tsx
+++ frontend1/src/App.tsx
@@ -1,7 +1,18 @@
-import ListGroup from "./components/ListGroup";
 
-function App () {
-  return <div><ListGroup /></div>
+import React from 'react';
+
+function App() {
+  const appStyle = {
+    backgroundColor: '#00FF00', // Set the background color to green
+    minHeight: '100vh', // Ensure the app takes at least the full height of the viewport
+    padding: '20px', // Add some padding for better visibility
+  };
+
+  return (
+    <div style={appStyle}>
+      {/* Your app content goes here */}
+    </div>
+  );
 }
 
 export default App;
--- frontend1/src/components/ListGroup.tsx
+++ frontend1/src/components/ListGroup.tsx
@@ -1,5 +1,5 @@
 function ListGroup() {
-    return <h1>List Group</h1>;
+    return <h1>Team Builder</h1>;
 }
 
 export default ListGroup;
