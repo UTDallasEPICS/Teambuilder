90ba7677a1144bed4807f4c81e0df4cfaccace89
jeremiahb <jbthelegend15@gmail.com>
2024-02-22 00:05:01 -0600

    added image logo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,26 @@
 import styles from './page.module.css';
-
+import Head from 'next/head';
 
 export default function Dashboard() {
   return (
-    <div className={styles.background}>
-      <div>
-        <h1>EPICS Team Builder</h1>
+    <div>
+      <div className={styles.background}>
+        <Head>
+          <title>Dashboard</title>
+        </Head>
+        
+        <img src="logo.png" alt="EPICS Logo" width="100"/>
+        <br></br>
+        <br></br>
+        <div>
+          <h1>EPICS Team Builder</h1>
+        </div>
+        <br></br>
+        <div>
+          <h2>Dashboard</h2>
+        </div>
+      
       </div>
-      <br></br>
-      <div>
-        <h2>Dashboard</h2>
-      </div>
-    </div>
+     </div>
   );
 }
