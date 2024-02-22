8b45c071fa47b25212de0b61f5822cca2f11446e
jbthelegend <jbthelegend15@gmail.com>
2024-02-21 18:04:49 -0600

    2/21, css, changed background to green

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.css';
+
 
 export default function Dashboard() {
-  return (<p> Hello lol  </p>
-  )
+  return (
+    <div className={styles.background}>
+      <div>
+        <h1>EPICS Team Builder</h1>
+      </div>
+      <br></br>
+      <div>
+        <h2>Dashboard</h2>
+      </div>
+    </div>
+  );
 }
