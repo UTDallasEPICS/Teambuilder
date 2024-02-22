9c60f7f8a6e568f4e8b391f7912de7c1c88ef0bc
jeremiahb <jbthelegend15@gmail.com>
2024-02-21 22:54:04 -0600

    asd

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Dashboard() {
+export default function Project() {
   return (<p> Hello lol  </p>
   )
 }
