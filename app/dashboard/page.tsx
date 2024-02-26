c6fae06393c89e40d24a3c9758e4918e80cc9c0b
Zuluwudududupuu <syedkabir909@gmail.com>
2024-02-26 03:09:20 -0600

    Added background for Instruction and Start Upload button
    This was an initial Instruction commit and test.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,9 +21,14 @@ export default function Dashboard() {
 
           </div>
         </div>
+
+        <div className='border-solid border-2 border-sky-500 rounded-3xl bg-cyan-500 min-h-44 mt-1 mr-3 ml-3 mb-3'>
+          <h1>Instruction</h1>
+        </div>
+
           <br></br>
           <div>
-          <button>Student List Upload</button>
+          <button className='border-solid border-2 border-sky-500 rounded-lg bg-cyan-500'>Student List Upload</button>
           </div>
 
           <br></br>
