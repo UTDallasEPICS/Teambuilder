530782ee5763943735291e2d14e11c0591c55418
Zuluwudududupuu <syedkabir909@gmail.com>
2024-02-27 12:11:36 -0600

    Added CSS for the large outer box, Student List Upload button, and Form Teams >> button.
    The element alignments and margins will need to be changed to allow flex and layout for the Student List and Project List Review.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import '../globals.css';
 
 export default function Dashboard() {
   return ( 
-    <div className={styles.backgroundFont}>
+    <div className={styles.backgroundFont}> 
       <div>
         <Head>
           <title>Dashboard</title>
@@ -20,22 +20,26 @@ export default function Dashboard() {
           </div>
 
           </div>
-        </div>
+      </div>
 
-        <div className='border-solid rounded-3xl min-h-44 mt-1 mr-3 ml-5 mb-3 bg-[rgba(48,100,162,0.29)]  flex flex-col'>
+      <div className='border-solid rounded-t-3xl box-border ml-3 mr-3 bg-[rgba(90,91,88,0.49)] pl-4 pt-7'>
+
+        <div className='box-border border-solid rounded-3xl min-h-44 mt-1 mr-5 ml-5 mb-3 bg-[rgba(48,100,162,0.29)]  flex flex-col'>
           <h1 className='ml-5 mt-3 text-xl'>Instruction</h1>
           <h2 className='ml-5 mt-2'>Upload your student list here as an excel sheet. make sure you have these columns: blah blah blah. Thxxxx</h2>
         </div>
 
-          <br></br>
-          <div>
-          <button className='border-solid border-2 border-sky-500 rounded-lg bg-cyan-500'>Student List Upload</button>
-          </div>
+        <br></br>
+        <div>
+          <button className='ml-5 border-solid border-1 p-1 border-transparent rounded-xl bg-[rgba(96,241,135,0.9)]'>Student List Upload</button>
+        </div>
 
-          <br></br>
-          <div>
-            <button>Form Teams &gt;&gt;</button>
-          </div>
+        <br></br>
+        <div>
+          <button className='mt-20 ml-5 border-solid border-1 p-2 border-transparent rounded-xl bg-[rgba(96,241,135,0.9)] text-xl'>Form Teams &gt;&gt;</button>
+        </div>
+
+      </div>
     </div>
       
   );
