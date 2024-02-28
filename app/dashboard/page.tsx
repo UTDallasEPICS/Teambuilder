2c53558be72a87def8dead161a1def77446288de
meganchacko <79389137+meganchacko@users.noreply.github.com>
2024-02-27 20:23:52 -0600

    worked on display page, made background, boxes, and some of the starting text.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -24,9 +24,9 @@ export default function Dashboard() {
 
       <div className='border-solid rounded-t-3xl box-border ml-3 mr-3 bg-[rgba(90,91,88,0.49)] pl-4 pt-7'>
 
-        <div className='box-border border-solid rounded-3xl min-h-44 mt-1 mr-5 ml-5 mb-3 bg-[rgba(48,100,162,0.29)]  flex flex-col'>
-          <h1 className='ml-5 mt-3 text-xl'>Instruction</h1>
-          <h2 className='ml-5 mt-2'>Upload your student list here as an excel sheet. make sure you have these columns: blah blah blah. Thxxxx</h2>
+        <div className='box-border border-solid rounded-3xl min-h-44 mt-1 mr-9 ml-5 mb-3 bg-[rgba(48,100,162,0.29)]  flex flex-col'>
+          <h1 className='ml-4 mt-3 text-xl'>Instruction</h1>
+          <h2 className='ml-4 mt-2 mr-3'>Upload your student list here as an excel sheet. make sure you have these columns: blah blah blah. Thxxxx</h2>
         </div>
 
         <br></br>
@@ -38,7 +38,7 @@ export default function Dashboard() {
         <div>
           <button className='mt-20 ml-5 border-solid border-1 p-2 border-transparent rounded-xl bg-[rgba(96,241,135,0.9)] text-xl'>Form Teams &gt;&gt;</button>
         </div>
-
+      
       </div>
     </div>
       
--- app/display/page.tsx
+++ app/display/page.tsx
@@ -1,17 +1,39 @@
 import styles from './page.module.css';
-
+import Head from 'next/head';
+import '../globals.css';
 
 export default function Display() {
   return (
-    <div className={styles.background}>
+    <div className={styles.backgroundFont}>
       <div>
-        <h1>EPICS Team Builder</h1>
-      </div>
-      <br></br>
-      <div>
-        <h2>Results Display</h2>
-      </div>
+      <Head>
+        <title>Display</title>
+      </Head>
       
+        <div className="flex">
+          <img className='object-left-top object-contain h-32 w-64' src="logo1.png" alt="EPICS Logo" width="100"/>
+          <div>
+          <img className='object-left object-contain h-32 w-64' src="team-formation-text.png" alt="Team FOrmation Logo" width="100"/>
+          </div>
+          <div>
+            <h2>Display</h2>
+          </div>
+        </div>
+      </div>
+
+  
+      <div className='border-solid rounded-t-3xl box-border ml-3 mr-3 bg-[rgba(90,91,88,0.49)] pl-4 pt-7 pb-11'>
+
+      <div className = "p-8 ml-4 mt-3 mr-7 bg-[rgba(90,91,88,0.49)] rounded-xl flex flex-col">
+            <div className = "mt-0.2 text-lg text-white font-medium">
+              <h1>Attribute Importance: </h1>
+            </div>
+            <div className = " mt-6 text-lg text-white font-medium">
+              <h1>Maximum Team Number: </h1>
+            </div>
+        </div>
+      </div>
+
     </div>
   );
 }
