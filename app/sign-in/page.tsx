7b5a1df62c5fdbcb5912e6da0b0affefe8e2fcab
saanvi lukka <155501947+saanvilukka@users.noreply.github.com>
2024-03-07 15:52:55 -0600

    sing in css

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,10 +1,11 @@
-import styles from './page.module.css';
+import './page.css';
 import Head from 'next/head';
 import '../globals.css';
 
 export default function SignIn () {
   return (
   <div> Login </div>
+
 
     
    
