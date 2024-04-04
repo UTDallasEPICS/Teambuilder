dd9f044370fe6573d5eb67073ed6b187cbe8fc8e
Shraddha Subash <shraddhasubashpublic@gmail.com>
2024-04-04 14:51:16 -0500

    added upload functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,12 +7,14 @@ const Navbar = () => {
       <div className="w-full h-20 bg-blue-700 bg-opacity-40 sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
+            
             <div className="flex">
               <img className='object-left-top object-contain h-20 w-64' src="logo1.png" alt="EPICS Logo" width="50"/>
               <div>
-                <img className='object-left object-contain  h-20 w-64' src="team-formation-text.png" alt="Team Formation Logo" width="100"/>
+                <img className='object-left npobject-contain  h-20 w-64' src="team-formation-text.png" alt="Team Formation Logo" width="100"/>
               </div>
             </div>
+
             <ul className="hidden md:flex gap-x-6 text-white">
               <li>
                 <Link href="/dashboard">
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -4,10 +4,10 @@ import '../globals.css';
 
 export default function Project() {
   return (
-    <div className={styles.background}>
+    <div >
       
       <br></br>
-      <div className='border-solid rounded-t-3xl box-border ml-3 mr-3 bg-[rgba(90,91,88,0.49)] pl-4 pt-7'>
+      <div className='h-screen border-solid rounded-t-3xl box-border m-10 bg-[rgba(90,91,88,0.49)] pl-4 pt-7 '>
         <div className='box-border border-solid rounded-3xl min-h-44 mt-1 mr-9 ml-5 mb-3 bg-[rgba(48,100,162,0.29)]  flex flex-col'>
             <h1 className='ml-4 mt-3 text-xl'>Instruction</h1>
             <h2 className='ml-4 mt-2 mr-3'>Upload your project information here. Ensure to enter project name, descirption, semester, and whether it is a new or returning project. Once you are ready, click next.</h2>
