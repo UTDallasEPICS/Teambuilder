5a7f738f980e2ba48e2cf10b941b69a64b9eb295
Syed Kabir <syedkabir909@gmail.com>
2024-04-16 13:20:46 -0500

    Reconfigured the project list UI - might need to delete the Projects box in the Projects Page as there are box size conflicts

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -1,14 +1,15 @@
 import React  from "react";
+import "./table.css"
 
 import {BsFillTrashFill, BsFillPencilFill} from "react-icons/bs";
 
 export const Table = () => {
-    return <div className=" m-0 pl-7 pt-2  bg-[rgba(48,100,162,0.29)] w-full">
-        <table style={{display: 'block', }}>
-            <thead>
+    return <div className=" m-0 pl-7 pt-2  bg-[rgba(48,100,162,0.29)] w-full"> 
+        <table style={{display: 'block', overflow: 'hidden', tableLayout: 'auto', borderCollapse: 'collapse', borderRadius: '10px', whiteSpace: 'nowrap', maxWidth: '100%', margin: 'auto', overflowX: 'auto' }}>
+            <thead style={{backgroundColor: '#ccc', color: '#222'}}>
                 <tr>
                     <th>Project Number</th>
-                    <th>Description</th>
+                    <th className="expand">Description</th>
                     <th>Actions</th>
                 </tr>
             </thead>
@@ -17,11 +18,11 @@ export const Table = () => {
                     <td>Project 1</td>
                     <td>This is the first project</td>
                     <td>
-                        <span>Complete</span>
+                        <span className="label label-complete">Complete</span>
                     </td>
                     <td>
-                        <span>
-                            <BsFillTrashFill />
+                        <span className="actions">
+                            <BsFillTrashFill className="delete-btn"/>
                             <BsFillPencilFill />
                         </span>
                     </td>
@@ -30,11 +31,11 @@ export const Table = () => {
                     <td>Project 2</td>
                     <td>This is the second project</td>
                     <td>
-                        <span>Incomplete</span>
+                        <span className="label label-incomplete">Incomplete</span>
                     </td>
                     <td>
-                        <span>
-                            <BsFillTrashFill />
+                        <span className="actions">
+                            <BsFillTrashFill className="delete-btn"/>
                             <BsFillPencilFill />
                         </span>
                     </td>
@@ -43,11 +44,11 @@ export const Table = () => {
                     <td>Project 3</td>
                     <td>This is the third project</td>
                     <td>
-                        <span>Incomplete</span>
+                        <span className="label label-incomplete">Incomplete</span>
                     </td>
                     <td>
-                        <span>
-                            <BsFillTrashFill />
+                        <span className="actions">
+                            <BsFillTrashFill className="delete-btn"/>
                             <BsFillPencilFill />
                         </span>
                     </td>
