649152958393056640054618e8e62092d6172f3c
Syed Kabir <syedkabir909@gmail.com>
2024-05-02 15:28:40 -0500

    Fixed min height of outline, removed z index of modal.css

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -51,7 +51,7 @@ export default function Project() {
     <div >
       
       <br></br>
-      <div className='h-screen border-solid rounded-t-3xl box-border m-10 bg-[rgba(90,91,88,0.49)] pl-4 pt-7 '>
+      <div style={{minHeight: '780px', borderRadius:'24px' }} className='h-screen border-solid rounded-t-3xl box-border m-10 bg-[rgba(90,91,88,0.49)] pl-4 pt-7 '>
         <div className='box-border border-solid rounded-3xl min-h-28 mt-1 mr-9 ml-5 mb-3 bg-[rgba(48,100,162,0.29)]  flex flex-col'>
             <h1 className='ml-4 mt-3 text-xl'>Instruction</h1>
             <h2 className='ml-4 mt-2 mr-3'>Upload your project information here. Ensure to enter project name, descirption, semester, and whether it is a new or returning project. Once you are ready, click next.</h2>
@@ -70,7 +70,7 @@ export default function Project() {
           
             <div className='flex-col'>
               <h1 style={{fontSize: '30px'}} className=' ml-16 mt-3 '>Edit Project</h1>
-              <div style={{ marginLeft: '50px' , width: '625px', minHeight: '300px', marginRight: '25px'}} className='customMargin box-border border-solid rounded-3xl  bg-[rgba(48,100,162,0.29)]  flex flex-col'>
+              <div style={{ marginLeft: '50px' , width: '475px', minHeight: '300px', marginRight: '25px'}} className='customMargin box-border border-solid rounded-3xl  bg-[rgba(48,100,162,0.29)]  flex flex-col'>
                 {modalOpen && (
                   <Modal 
                     closeModal ={() => {
