8b8534a6cd15a02dd708b801d5af067313ca1969
jbthelegend <jbthelegend15@gmail.com>
2024-05-02 17:06:38 -0500

    fixed error in page.tsx

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -51,44 +51,16 @@ export default function Project() {
           <h1 className="ml-4 mt-3 text-xl">Instruction</h1>
           <h2 className="ml-4 mt-2 mr-3">
             Upload your project information here. Be sure to enter project name,
-            project partner, target # of CS majors, and whether it is an
-            archived project. Once you are ready, click Submit.
+            project partner, target # of CS majors, and whether it is an archived project.
+            Once you are ready, click Submit.
           </h2>
         </div>
         <br></br>
-<<<<<<< Updated upstream
         <div style={{ marginLeft: "0px" }} className="flex">
           <div className="flex-col">
             <h1 style={{ fontSize: "30px" }} className=" ml-8 mt-3 ">
               Projects
             </h1>
-=======
-        <div style={{marginLeft: '0px' }} className='flex'>
-            <div className='flex-col'>
-              <h1 style={{fontSize: '30px'}} className=' ml-8 mt-3 '>Projects</h1>
-              
-                
-                  <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow}/>
-                
-            <button style={{display: 'block', margin: 'auto', marginTop: '1rem', border: 'none', backgroundColor: 'aqua', color: 'white', padding: '0.5rem 1rem', borderRadius: '10px', cursor: 'pointer', boxShadow: '0px 5px 5px #ccc' }} onClick={() => setModalOpen(true)}>Add</button>
-            </div>
-            
-          
-            <div className='flex-col'>
-              <h1 style={{fontSize: '30px'}} className=' ml-16 mt-3 '>Edit Project</h1>
-              <div style={{ marginLeft: '50px' , width: '570px', minHeight: '300px', marginRight: '25px'}} className='customMargin box-border border-solid rounded-3xl  bg-[rgba(48,100,162,0.29)]  flex flex-col'>
-                {modalOpen && (
-                  <Modal 
-                    closeModal ={() => {
-                      setModalOpen(false);
-                      setRowToEdit(null);
-                    }}
-                    onSubmit={handleSubmit}
-                    defaultValue={rowToEdit !== null && rows[rowToEdit]}
-                  />
-                )}
-              </div>
->>>>>>> Stashed changes
 
             <Table
               rows={rows}
