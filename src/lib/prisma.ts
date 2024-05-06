2f77f8a17e82e6681182a3a29c30868501f0b7a9
jeremiahb <jbthelegend15@gmail.com>
2024-05-06 15:10:00 -0500

    added prisma.ts

---
+++ src/lib/prisma.ts
@@ -0,0 +1,15 @@
+// src/lib/prisma.ts
+
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
