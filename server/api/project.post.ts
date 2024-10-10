3d3d76508c849a861d8556be2d24058f5dde1f5a
AndrewN20 <andrewnnormal@gmail.com>
2024-10-10 15:36:50 -0500

    Imported partnerPost to project.post.ts

--- server/api/project.post.ts
+++ server/api/project.post.ts
@@ -1,9 +1,11 @@
 // TODO: need to do partner connection
+import partnerPost from "./partner.post";
 export default defineEventHandler(async event => {
   const { name } = await readBody(event);
   return await event.context.client.project.create({
       data: {
        name,
       },
+      partnerPost
     });
 });
--- server/api/team.post.ts
+++ server/api/team.post.ts
@@ -1,10 +1,29 @@
 // TODO: extract list of students and do the join
 // TODO: also connect to project
-export default defineEventHandler(async event => {
+/*export default defineEventHandler(async event => {
   const { name } = await readBody(event);
   return event.context.client.team.create({
     data: {
       name,
     },
   });
+});*/
+
+export default defineEventHandler(async event => {
+  const { name } = await readBody(event);
+  const team = await event.context.client.team.create({
+    data: {
+      name,
+      project: {
+        connect: {
+          id: projectId, // Assuming projectId is provided in the request body
+        },
+      },
+      students: {
+        connect: studentIds.map(id => ({ id })), // Connect students using the array of student IDs
+      },
+    },
+  });
+
+  return team; // Return the created team object
 });
