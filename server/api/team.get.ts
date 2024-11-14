1517733abcfd3fe16a5860046822a188d4700a8c
Andrei Favorito <77313875+andreif3112@users.noreply.github.com>
2024-11-14 16:26:39 -0600

    11/14/24 - Finished implementation of team.get.ts (1)?

--- server/api/team.get.ts
+++ server/api/team.get.ts
@@ -1,8 +1,24 @@
-export default defineEventHandler(async event => {
+// export default defineEventHandler(async event => {
+//   const { id } = getQuery(event);
+//   return await event.context.client.team.findFirst({
+//     where: {
+//       id
+//     }
+//   });
+// });
+
+export default defineEventHandler(async (event) => {
   const { id } = getQuery(event);
+  
   return await event.context.client.team.findFirst({
     where: {
-      id
-    }
+      id,
+    },
+    include: {
+      Project: true,   // Include project details
+      Partner: true,   // Include partner details
+      Students: true,  // Include associated students
+    },
   });
 });
+
