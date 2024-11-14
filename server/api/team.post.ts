9375a610d60a5a42eb44e7555237c25963a77de4
Andrei Favorito <77313875+andreif3112@users.noreply.github.com>
2024-11-14 16:23:20 -0600

    11/14/24 - Finished implementation of team.post.ts (1)?

--- server/api/team.post.ts
+++ server/api/team.post.ts
@@ -2,12 +2,18 @@
 // TODO: also connect to project
 
 export default defineEventHandler(async (event) => {
-  const { name, partnerData, project } = await readBody(event);
+  const { name, partnerData, project, studentIds } = await readBody(event);
   const team = await event.context.client.team.create({
     data: {
       name, // Team name
       project: { // Connect to the project
         connect: { id: project, partnerData }
+      },
+      partner: {
+        connect: {  id: partnerData }
+      },
+      Students: {
+        connect: {  id: studentIds.map((studentId: string) => ({id: studentId}) )}
       }
     },
     include: {
