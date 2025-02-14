a93bd33f1ec41c09d2020cf5dd8b20032dee362e
Drew Layton <dlayton66@gmail.com>
2025-02-14 11:06:32 -0600

    feat: add hover effect to project cards

--- components/ProjectCardDisplay.vue
+++ components/ProjectCardDisplay.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .project-card.rounded-2xl.p-4.mb-3.mx-2(class="bg-[#FFF8DC]")
+  div.rounded-2xl.p-4.mb-3.mx-2.cursor-pointer(class="bg-[#FFF8DC] hover:translate-y-[-5px] hover:shadow-lg transition-transform duration-200")
     h2.text-xl.font-light(class="text-[#7BA7A6]") {{ title }}
     .indicators.flex.gap-2.mt-2
       .indicator(
@@ -8,15 +8,15 @@
       ) {{ indicator.toUpperCase() }}
       .semester(class="bg-[#E560F1] rounded-full px-3 py-1 text-xs font-light text-white") {{ sem }}
     p.text-sm.mt-2.font-light(class="text-[#7BA7A6]") {{ content }}
-  </template>
+</template>
   
-  <script setup lang="ts">
-  interface Props {
-    title: string
-    content: string
-    indicator: string
-    sem: string
-  }
-  
-  defineProps<Props>()
-  </script>
+<script setup lang="ts">
+interface Props {
+  title: string
+  content: string
+  indicator: string
+  sem: string
+}
+
+defineProps<Props>()
+</script>
