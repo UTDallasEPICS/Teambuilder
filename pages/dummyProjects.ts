0167a6fe8ef75cbaf0783b42f36512f0deedaa71
Drew Layton <dlayton66@gmail.com>
2025-02-14 11:13:04 -0600

    refactor: moves dummy project data to separate file

---
+++ pages/dummyProjects.ts
@@ -0,0 +1,104 @@
+// plz remove this file once it's connected to real data
+
+import type { Project } from "../types";
+
+export const dummyProjects: Project[] = [
+  {
+    id: 1,
+    title: 'Patient Data Collection App',
+    content: 'A new app for collecting patient data.',
+    indicator: 'New',
+    sem: 'S2024'
+  },
+  {
+    id: 2,
+    title: 'Automated Family Page',
+    content: 'A project to automate the generation of family pages.',
+    indicator: 'returning',
+    sem: 'S2024'
+  },
+  {
+    id: 3,
+    title: 'Communication App',
+    content: 'A real-time communication app for team collaboration.',
+    indicator: 'new',
+    sem: 'F2023'
+  },
+  {
+    id: 4,
+    title: 'Data Collection App',
+    content: 'A new app for collecting patient data.',
+    indicator: 'New',
+    sem: 'S2024'
+  },
+  {
+    id: 5,
+    title: 'Patient Data Collection App',
+    content: 'A new app for collecting patient data.',
+    indicator: 'New',
+    sem: 'S2024'
+  },
+  {
+    id: 6,
+    title: 'Automated Family Page',
+    content: 'A project to automate the generation of family pages.',
+    indicator: 'returning',
+    sem: 'S2024'
+  },
+  {
+    id: 7,
+    title: 'Communication App',
+    content: 'A real-time communication app for team collaboration.',
+    indicator: 'new',
+    sem: 'F2023'
+  },
+  {
+    id: 8,
+    title: 'Data Collection App',
+    content: 'A new app for collecting patient data.',
+    indicator: 'New',
+    sem: 'S2024'
+  },
+  {
+    id: 9,
+    title: 'Patient Data Collection App',
+    content: 'A new app for collecting patient data.',
+    indicator: 'New',
+    sem: 'S2024'
+  },
+  {
+    id: 10,
+    title: 'Automated Family Page',
+    content: 'A project to automate the generation of family pages.',
+    indicator: 'returning',
+    sem: 'S2024'
+  },
+  {
+    id: 11,
+    title: 'Communication App',
+    content: 'A real-time communication app for team collaboration.',
+    indicator: 'new',
+    sem: 'F2023'
+  },
+  {
+    id: 12,
+    title: 'Patient Data Collection App',
+    content: 'A new app for collecting patient data.',
+    indicator: 'New',
+    sem: 'S2024'
+  },
+  {
+    id: 13,
+    title: 'Automated Family Page',
+    content: 'A project to automate the generation of family pages.',
+    indicator: 'returning',
+    sem: 'S2024'
+  },
+  {
+    id: 14,
+    title: 'Communication App',
+    content: 'A real-time communication app for team collaboration.',
+    indicator: 'new',
+    sem: 'F2023'
+  },
+];
