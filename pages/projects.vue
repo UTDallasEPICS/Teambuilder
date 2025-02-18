02991661c39024d40e7601ce98404ff7b62f0908
Drew Layton <dlayton66@gmail.com>
2025-02-18 14:42:24 -0600

    refactor/style: past-projects, projects, and students

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,10 +1,14 @@
 <template lang="pug">
   .centered-rows.shaded-card.p-10.mx-96.mt-10.gap-10(class="h-[80vh]")
     .centered-cols.relative.size-full
-      .text-7xl.drop-shadow-md Past Projects
+      .flex.absolute.top-0.left-5.gap-2
+        FileUploadButton(title="Upload Projects" @fileSelected="handleParsed")
+        HelpIcon(:info="helpInfo")
+
+      .text-7xl.drop-shadow-md Projects
       .text-2xl.drop-shadow-md.mt-2 Project Count: {{ filteredProjectCount }}
 
-      .grid.grid-cols-3.teal-card.p-10.mt-10.gap-3.h-full.w-full.overflow-y-auto.no-scrollbar(style="grid-template-rows: repeat(auto-fill, 165px);")
+      .grid.grid-cols-3.teal-card.p-10.mt-10.gap-3.size-full.overflow-y-auto.no-scrollbar(style="grid-template-rows: repeat(auto-fill, 165px);")
         ProjectCardDisplay(
           v-for="project in filteredProjects"
           :key="project.id"
@@ -18,12 +22,16 @@
           .cardTitle Filters
           .flex.flex-col.gap-2
             .cardSubTitle Search by Name
-            input(
+            input.text-teal.p-2.rounded(
               v-model="searchQuery"
               type="text"
-              class="border text-teal p-2 rounded w-full mb-4"
             )
           .flex.gap-10
+            div
+              .cardSubTitle Status
+              .flex.flex-col.gap-2.mt-1
+                button.pill.w-fit(@click="toggleFilterByNew" :class="newStatusBgColor") NEW
+                button.pill.w-fit(@click="toggleFilterByReturning" :class="returningStatusBgColor") RETURNING
             div
               .cardSubTitle Semester
               .grid.grid-flow-col.grid-rows-3.gap-2.mt-1
@@ -52,29 +60,32 @@
             span.cardSubTitle Repo:
             a.cardText(:href="selectedProject?.repoURL" target="_blank") {{ selectedProject?.repoURL }}
           .flex-grow.flex.justify-end.items-end
-            Button(v-if="selectedProject" title="Restore Project" type="success" @click="handleRestore")
-
+            Button(v-if="selectedProject" title="Archive Project" type="warning" @click="handleArchive")
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import type { Project } from '../types';
 import { useHead } from '@unhead/vue'
 import { storeToRefs } from 'pinia';
-import { useProjectFilters } from '../composables/project/useProjectFilters';
 import { useProjectStore } from '../stores/projectStore';
+import { useProjectFilters } from '../composables/project/useProjectFilters';
 
 useHead({ title: 'Projects' });
 
 const projectStore = useProjectStore();
-const { getArchivedProjects: projects } = storeToRefs(projectStore);
+const { getActiveProjects: projects } = storeToRefs(projectStore);
 const selectedProject = ref<Project | null>(null);
 
 const {
   searchQuery,
+  filterByNew,
+  filterByReturning,
   semesterFilters,
   filteredProjects,
   toggleSemesterFilter,
+  toggleFilterByNew,
+  toggleFilterByReturning,
   filteredProjectCount
 } = useProjectFilters(projects);
 
@@ -82,13 +93,29 @@ const selectProject = (project: Project) => {
   selectedProject.value = project;
 }
 
+const handleArchive = () => {
+  const id = selectedProject.value?.id;
+  if (id) projectStore.archiveProject(id);
+}
+
+const handleParsed = (parsed: any) => {
+  console.log(parsed)
+};
+
+const newStatusBgColor = computed(() => 
+  filterByNew.value ? "bg-green" : "bg-gray-500"
+)
+
+const returningStatusBgColor = computed(() => 
+  filterByReturning.value ? "bg-orange" : "bg-gray-500"
+)
+
 const semesterBgColor = (semester: string) => 
   semesterFilters[semester] ? "bg-magenta" : "bg-gray-500";
 
-const handleRestore = () => {
-  const id = selectedProject.value?.id;
-  if (id) projectStore.restoreProject(id);
-}
+const helpInfo = `Upload information for your projects here.  
+  Be sure to enter project name, project partner, target # of CS majors, 
+  and whether it is an archived project.`
 </script>
 
 <style scoped>
--- pages/students.vue
+++ pages/students.vue
@@ -1,166 +1,48 @@
 <template lang="pug">
-  // this all needs to be a students page, not on the index - index should be demographics display
-  div.backgroundFont.text-pillText
-    div(style='padding-top: 10px; margin: 10px;' class='pt-10 h-screen border-solid rounded-3xl box-border m-10 bg-[rgba(90,91,88,0.33)]')
-      div(class='box-border border-solid rounded-3xl min-h-28 mt-1 mr-9 ml-5 mb-3 bg-[rgba(48,100,162,0.29)]  flex flex-col text-pillText')
-        h1.ml-4.mt-3.text-xl Instruction
+  .centered-rows
+    .centered-cols.shaded-card.m-10.relative(class="max-w-[1200px]")
+      .text-7xl.drop-shadow-md Students
 
-        h2.ml-4.mt-2.mr-3 Upload your student list here as an excel sheet. Make sure you have these columns in your CSV file: Student ID, First Name, Last Name, Major, Seniority, and Project Preferences.
-  
-        
-      div(style='marginTop: "50px"' class='flex flex-inline')
-        div(class = "flex flex-col")
-          FileUploadButton(title="Student List Upload" @fileSelected="handleFile" ref="fileUploader")
+      .text-2xl.drop-shadow-md.mt-2 Student Count: {{ studentCount }}
 
-          div(style='maxWidth: "210px", minHeight: "30px", padding:"3px"' class='bg-[rgb(102,103,104)] mt-3 ml-6 mb-3 rounded-md') 
-            p(v-if='fileName' style='color: "white", fontSize: "11px", margin: "3px", wordWrap: "break-word", width: "100%"') {{ fileName }}
-            
-          div(class="flex justify-start mt-32 ml-10")
-            NuxtLink(to='/generate')
-              button(
-                class='border-solid border-8 p-2 border-transparent rounded-xl bg-[rgba(96,241,135,0.9)] text-xl'
-              ) Form Teams &gt;&gt;
-        
-        //This is the parts where I need to add a list component to them
-        div(style={ display: 'flex', flexDirection: 'column', marginLeft: '100px' } class='flex')
-          // Header Row
-          div(style={ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' })
-            h1(class='text-white text-3xl ml-24') Project List
-            h1(class='text-white text-3xl ml-48') Student List Preview
-            p(class='text-white text-xl') Student Count: {{ studentCount }}
+      .flex.absolute.top-5.right-5.gap-2
+        HelpIcon
 
-          // Scrollable Components Row
-          div(style={ display: 'flex', gap: '20px' }) 
-            // Projects Scroll Component
-            div.border-solid.rounded-3xl.box-border(
-              style="height: 450px; width: 350px; overflow-y: scroll;" 
-              class="bg-[rgba(48,100,162,0.29)] p-4 pt-6"
-            )
-              .project-container(class="space-y-4")
-                ProjectCardDisplay(
-                  v-for="project in projects" 
-                  :title="project.title"
-                  :content="project.content"
-                  :indicator="project.indicator"
-                  :sem="project.sem"
-                )
-            
-            // Student List Scroll Component
-            div.border-solid.rounded-3xl.box-border.ml-10(
-              v-if="maxTeamSize" 
-              style="height: 450px; width: 600px; overflow-y: scroll;"
-              class="bg-[rgba(48,100,162,0.29)] p-4 pt-6"
-            )
-              .student-container(class="space-y-4")
-                StudentCardDisplay(
-                  v-for="student in students"
-                  :lname="student.lname"
-                  :fname="student.fname"
-                  :indicator="student.indicator"
-                  :netID="student.netID"
-                  :isAssigned="student.isAssigned"
-                )
+        FileUploadButton(title="Upload Students" @fileSelected="handleParsed")
 
-            //div
-              ListComponent(:items="itemsList")
-        
-  </template>
-  
-  <script setup lang="ts">
-  const maxTeamSize = ref(5); 
-  import ProjectCardDisplay from '~/components/ProjectCardDisplay.vue';
-  import StudentCardDisplay from '~/components/StudentCardDisplay.vue';
-  
-  const fileName = ref("");
-  const uploaded = ref(false);
+      .grid.grid-cols-4.teal-card.mt-10.gap-3
+        StudentCardDisplay(
+          v-for="student in students"
+          :key="student.id"
+          v-bind="student"
+        )
+</template>
 
-  const fileUploader = ref<FileUploaderInstance | null>(null);
+<script lang="ts" setup>
+import { useHead } from '@unhead/vue'
+import { useStudentStore } from '../stores/studentStore';
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+import { useStudentFilters } from '../composables/student/useStudentFilters';
+import type { Student } from '../types';
 
-  interface FileUploaderInstance {
-    handleClick: () => void;
-  }
+useHead({ title: 'Students' });
 
-  const handleFile = (file: File) => {
-    // Trigger file input click when the button is clicked
-    fileName.value = file.name;
-    uploaded.value = true;
-  };
-  const triggerFileUpload = () => {
-  // Call the handleClick method of the FileUploader
-    fileUploader.value?.handleClick(); // This will trigger the file input in FileUploader
-  };
+const studentStore = useStudentStore();
+const { getAllStudents: students } = storeToRefs(studentStore);
+const selectedStudent = ref<Student | null>(null);
 
-  const projects = ref([
-    {
-      title: 'Patient Data Collection App',
-      content: 'A new app for collecting patient data.',
-      indicator: 'New',
-      sem: 'S2024'
-    },
-    {
-      title: 'Automated Family Page',
-      content: 'A project to automate the generation of family pages.',
-      indicator: 'returning',
-      sem: 'S2024'
-    },
-    {
-      title: 'Communication App',
-      content: 'A real-time communication app for team collaboration.',
-      indicator: 'new',
-      sem: 'F2023'
-    },
-    {
-      title: 'Data Collection App',
-      content: 'A new app for collecting patient data.',
-      indicator: 'New',
-      sem: 'S2024'
-    }
-  ]);
+const {
+    searchQuery,
+    filterByNew,
+    filterByReturning,
+    filteredStudents,
+    filteredStudentCount,
+    toggleFilterByNew,
+    toggleFilterByReturning,
+  } = useStudentFilters(students);
 
-  const students = ref([
-    {
-      lname: 'Dan',
-      fname: 'Martin',
-      indicator: 'new',
-      netID: 'dxm220546',
-      isAssigned: true
-    },
-    {
-      lname: 'Chris',
-      fname: 'Colman',
-      indicator: 'returning',
-      netID: 'cxc220546',
-      isAssigned: false
-    },
-    {
-      lname: 'Subrahmanya',
-      fname: 'Mohanasundar',
-      indicator: 'new',
-      netID: 'sxm220546',
-      isAssigned: false
-    },
-    {
-      lname: 'Subrahmanya',
-      fname: 'Mohanasundar',
-      indicator: 'new',
-      netID: 'sxm220546',
-      isAssigned: false
-    },
-    {
-      lname: 'Subrahmanya',
-      fname: 'Mohanasundar',
-      indicator: 'new',
-      netID: 'sxm220546',
-      isAssigned: false
-    },
-    {
-      lname: 'Subrahmanya',
-      fname: 'Mohanasundar',
-      indicator: 'new',
-      netID: 'sxm220546',
-      isAssigned: false
-    }
-  ]);
-  const studentCount = computed(() => students.value.length);
-  </script>
-
+const handleParsed = (parsed: any) => {
+  console.log(parsed)
+};
+</script>
