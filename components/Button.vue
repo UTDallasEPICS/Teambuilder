6c1aee4b4f09cbf457387dd78f85b6a627f94337
Drew Layton <dlayton66@gmail.com>
2025-02-18 14:30:23 -0600

    feat: adds button component with 3 color options

--- components/Button.vue
+++ components/Button.vue
@@ -1,23 +1,46 @@
 <template lang="pug">
-button.edge.rounded-xl.cursor-pointer
-  span.front.px-6.py-3.text-xl.rounded-xl {{ title }}
+button.edge.rounded-xl.cursor-pointer.text-beige(:class="edgeClass")
+  .front.px-6.py-3.text-2xl.rounded-xl.embossed(:class="frontClass") {{ title }}
 </template>
     
 <script setup lang="ts">
-defineProps<{ title: string }>();
+const props = defineProps<{ 
+  title: string
+  type?: 'success' | 'warning';
+}>();
+
+const type = props.type || 'default';
+const edgeClass = type + 'Edge';
+const frontClass = type + 'Front';
 </script>
 
 <style>
 .edge {
-  background: #c2795d;
   outline-offset: 4px;
 }
 .front {
   display: block;
-  background: #FFA07A;
   transform: translateY(-5px);
+  text-shadow: 1px 1px 1px #0000008b;
 }
-
+.defaultEdge {
+  background: #c2795d;
+}
+.defaultFront {
+  background: #ffa07a;
+}
+.warningEdge {
+  background: #af5252;
+}
+.warningFront {
+  background: #eb6464;
+}
+.successEdge {
+  background: #43944c;
+}
+.successFront {
+  background: #77cf77;
+}
 .edge:active .front {
   transform: translateY(-2px);
 }
