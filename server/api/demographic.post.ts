81c5afaa34593edfbfa98208ecfb831815ae8f91
IsaacP <isaacphilo1@gmail.com>
2025-03-30 14:43:26 -0500

    Added another parsing package

--- server/api/demographic.post.ts
+++ server/api/demographic.post.ts
@@ -2,6 +2,8 @@
 //This API's purpose is to support adding to the database from a file upload
 
 import { PrismaClient } from "@prisma/client"
+import xlsx from 'node-xlsx';
+import * as XLSX from 'xlsx'; //This will be better used as https://docs.sheetjs.com/docs/getting-started/installation/nodejs#vendoring
 
 const prisma = new PrismaClient()
 
@@ -9,8 +11,8 @@ export default defineEventHandler(async (event) => {
   try {
     const postBody = await readBody(event);//Assuming this gets the whole file through in one go, like in https://nuxt.com/docs/guide/directory-structure/server#body-handling.
     //If that is not true, then this page may be helpful: https://austingil.com/file-uploads-in-node/
+    XLSX.utils.decode_range("J34:W57");
     
-
   }
   catch (error) {//borrowed from the GET API
     console.error(error);
