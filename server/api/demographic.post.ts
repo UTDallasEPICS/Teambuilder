5f9e960ff74b39bbe2327bacc646600654fa7647
IsaacP <isaacphilo1@gmail.com>
2025-04-01 14:59:27 -0500

    Changing the type of extracted data

--- server/api/demographic.post.ts
+++ server/api/demographic.post.ts
@@ -14,7 +14,7 @@ export default defineEventHandler(async (event) => {
     const relevantRange = XLSX.utils.decode_range("J34:W57"); //This may need to be horizontally extended to accomodate future semesters. However, there's no option to have an unspecified right boundary.
     //relevant tutorial page for remote file processing: https://docs.sheetjs.com/docs/solutions/input#example-remote-file
     const workbook = XLSX.read(postBody); //Assuming that the whole post body will be the file.
-    const chosenCells = XLSX.utils.sheet_to_json(workbook, {range: relevantRange});
+    const chosenCells = XLSX.utils.sheet_to_json(workbook, {header: 1, range: relevantRange});
     console.log(chosenCells);
   }
   catch (error) {//borrowed from the GET API
