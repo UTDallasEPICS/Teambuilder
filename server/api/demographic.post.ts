becc886b294f02d3e26dad7f16c7e73c064088e8
IsaacP <isaacphilo1@gmail.com>
2025-04-02 13:12:09 -0500

    Logic and implementation for extracting the relevant cells

--- server/api/demographic.post.ts
+++ server/api/demographic.post.ts
@@ -11,11 +11,22 @@ export default defineEventHandler(async (event) => {
   try {
     const postBody = await readBody(event);//Assuming this gets the whole file through in one go, like in https://nuxt.com/docs/guide/directory-structure/server#body-handling.
     //If that is not true, then this page may be helpful: https://austingil.com/file-uploads-in-node/
-    const relevantRange = XLSX.utils.decode_range("J34:W57"); //This may need to be horizontally extended to accomodate future semesters. However, there's no option to have an unspecified right boundary.
     //relevant tutorial page for remote file processing: https://docs.sheetjs.com/docs/solutions/input#example-remote-file
     const workbook = XLSX.read(postBody); //Assuming that the whole post body will be the file.
-    const chosenCells = XLSX.utils.sheet_to_json(workbook, {header: 1, range: relevantRange});
-    console.log(chosenCells);
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    
+    //const relevantRange = XLSX.utils.decode_range("J34:W57"); //This may need to be horizontally extended to accomodate future semesters. However, there's no option to have an unspecified right boundary.
+    const rangeSemesterNames = XLSX.utils.decode_range("L34:V34");
+    const rangeEthnicity = XLSX.utils.decode_range("K35:K44");
+    const semesterNames = extractCells(worksheet, rangeSemesterNames);
+    const ethnicities = extractColumnMajor(workbook, rangeEthnicity);
+
+    const range2200 = XLSX.utils.decode_range("L35:V45");
+    const range3200 = XLSX.utils.decode_range("L48:V56");
+    const dataFrom2200 = extractColumnMajor(worksheet, range2200);
+    const dataFrom3200 = extractColumnMajor(worksheet, range3200);
+    console.log(dataFrom2200);
+    console.log(dataFrom3200);
   }
   catch (error) {//borrowed from the GET API
     console.error(error);
@@ -25,3 +36,23 @@ export default defineEventHandler(async (event) => {
     }
   }
 })
+
+function getColumnMajor(arr: any[]){//Requires a rectangular 2D array
+    let transpose = [];
+    for(let c = 0; c < arr[0].length; c++){
+        let currentCol = [];
+        for(let r = 0; r < arr.length; r++){
+            currentCol.push(arr[r][c]);
+        }
+        transpose.push(currentCol); //Pushes it as, essentially, a row
+    }
+    return transpose;
+}
+
+function extractCells(worksheet: XLSX.WorkSheet, relevantRange: XLSX.Range){
+    return XLSX.utils.sheet_to_json(worksheet, {header: 1, range: relevantRange});
+}
+
+function extractColumnMajor(worksheet: XLSX.WorkSheet, relevantRange: XLSX.Range){
+    return getColumnMajor(extractCells(worksheet, relevantRange));
+}
