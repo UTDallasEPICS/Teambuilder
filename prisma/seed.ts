fb2acc6bbea8e47d5b57b231805c8d9d6e6be0ee
Drew Layton <dlayton66@gmail.com>
2025-04-03 16:12:07 -0500

    feat: add students to seeding and create seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from "@prisma/client"
 import { createRandomPartners } from "~/server/factories/partner";
 import { createRandomProjects } from "~/server/factories/project";
+import { createRandomStudents } from "~/server/factories/student";
+
 const prisma = new PrismaClient()
 
 const main = async () => {
     try {
         const partners = createRandomPartners(20);
         const projects = createRandomProjects(100, partners);
+        const students = createRandomStudents(200);
 
         await prisma.partner.createMany({ data: partners });
         await prisma.project.createMany({ data: projects });
+        await prisma.student.createMany({ data: students });
     } catch(error) {
         console.log(error)
     }
