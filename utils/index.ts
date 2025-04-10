cfd06c31c5eb30e7b7625bf4acfe5db60b4c9c46
Drew Layton <dlayton66@gmail.com>
2025-04-10 16:18:22 -0500

    feat: stringifySemesters

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,21 @@
+import type { Semester } from "@prisma/client";
+
 export const capitalize = (string: string | null) => {
   if (!string) return null;
   return string[0].toUpperCase() + string.substring(1).toLowerCase();
+}
+
+// TODO: get these ordered
+export const stringifySemesters = (semesters: Semester[] | undefined) => {
+  let string = '';
+  if (!semesters) return string;
+  
+  semesters.forEach(semester => {
+    string += capitalize(semester.season);
+    string += ' ';
+    string += semester.year;
+    string += ', ';
+  })
+
+  return string.slice(0, -2);
 }
