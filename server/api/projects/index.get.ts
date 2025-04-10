4589f0866ed3e23123cc05599b10a12246fcaeae
Drew Layton <dlayton66@gmail.com>
2025-04-10 16:19:14 -0500

    feat: update get all projects to include semester data

--- server/api/projects/index.get.ts
+++ server/api/projects/index.get.ts
@@ -1,3 +1,23 @@
+import { Prisma } from "@prisma/client";
+
 export default defineEventHandler(async (event) => {
-    return event.context.client.project.findMany();
+  return event.context.client.project.findMany({
+    include: {
+      teams: {
+        select: {
+          semester: true
+        }
+      }
+    }
+  });
 })
+
+export type ProjectWithSemesters = Prisma.ProjectGetPayload<{
+  include: {
+    teams: {
+      select: {
+        semester: true
+      }
+    }
+  }
+}>
