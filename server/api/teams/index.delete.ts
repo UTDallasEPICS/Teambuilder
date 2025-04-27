89d721005e6f9af1cd0184e8d068d063e2f7a499
Drew Layton <dlayton66@gmail.com>
2025-04-27 15:38:20 -0500

    feat: teams api calls

--- server/api/teams/index.delete.ts
+++ server/api/teams/index.delete.ts
@@ -1,11 +1,26 @@
+// Teams are deleted on the generate teams page.
+// This deletes multiple teams at once.
+// Expects an array of project ids & a semester id.
+// TODO: how to handle deleting a team with students already assigned?
+
+import { PrismaClient } from "@prisma/client";
+
 export default defineEventHandler(async event => {
-  const { id } = getQuery(event);
-  await event.context.client.team.update({
+  const client: PrismaClient = event.context.client;
+
+  const { semesterId, projectIds } = await readBody<TeamDeleteBody>(event);
+  
+  const deletedTeams = await client.team.deleteMany({
     where: {
-      id
-    },
-    data: {
-      removed:true,
-    },
+      semesterId,
+      projectId: { in: projectIds }
+    }
   });
+
+  return { status: 200, data: deletedTeams};
 });
+
+interface TeamDeleteBody {
+  semesterId: string;
+  projectIds: string[];
+}
