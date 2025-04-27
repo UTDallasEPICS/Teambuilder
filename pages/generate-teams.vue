3556e603df9b5ee7dd31ec5a0bdc36acd2b1bdd9
Drew Layton <dlayton66@gmail.com>
2025-04-27 15:36:46 -0500

    feat: activate semesters on generate teams page

--- pages/generate-teams.vue
+++ pages/generate-teams.vue
@@ -1,76 +1,208 @@
 <template lang="pug">
-  .centered-row.mt-10
-    .centered-col.gap-10
-      .shaded-card.centered-row.gap-10.p-10
-        .text-3xl.centered-col.teal-card.p-10
-          div Rank
-          div Attributes
-          ol.list-decimal.centered-col.gap-3.mt-5(ref="parent")
-            li.cursor-move.beige-card.text-xl(v-for="option in options" :key="option") {{ option }}
-        form.text-3xl.gap-4.centered-col
-          div Team Size:
-          input(
-            v-model="maxTeamSize"
-            type="text"
-            id="max_team_size"
-            class="w-40 bg-[rgba(255,255,255,0.96)] border border-gray-300 text-black text-sm rounded-lg p-2.5"
-            placeholder="Enter number"
-            required
-          )
-          ClickableButton(title="Generate Teams" @click="handleGenerateTeams")
-        
-      .centered-row.gap-10
-        .centered-col.shaded-card.p-4.text-xl
-          div Team Combos
-          .mb-3 Count: X  
-          CombinationCard(
-            :combination= 1
-            :sFactor= 8
-            :cFactor= 4
-            :pFactor= 7
-            :tWeight= 7
-            :unassignedStudents= 30 
-          )
-        
-        .centered-col.shaded-card.p-4.text-xl.gap-2
-          div Unassigned Students  
-          | Count: X  
-          div.gap-3.overflow-y-auto.h-full
-            StudentCardDisplay(
-              v-for="student in students"
-              :key="student.id"
-              v-bind="student"
-            )
-
-        .centered-col.shaded-card.p-4.text-xl.gap-2
-          div Projects
-          | Incomplete: X 
-          div.gap-3.overflow-y-auto.h-full
-        
-      ClickableButton(title="Export as CSV")
-  
+  .centered-col.shaded-card.mt-10.p-10.gap-10
+    .stepCard
+      .cardTitle Step 1: Start New Semester
+    .stepCard.gap-5
+      .cardTitle Step 2: Activate Projects for Semester
+      Dropdown(
+        v-model="selectedSemester"
+        :options="semesters"
+        placeholder="Select Semester"
+      )
+        template(#option="slotProps")
+          div(v-if="slotProps.option") {{ displaySemester(slotProps.option) }}
+          span(v-else) {{ slotProps.placeholder }}
+        template(#value="slotProps")
+          div(v-if="slotProps.value") {{ displaySemester(slotProps.value) }}
+          span(v-else) {{ slotProps.placeholder }}
+      .centered-row.gap-20.w-full
+        InputText(v-model="filterInactive" class="filterBox" size="small" placeholder="Search for Project")
+        InputText(v-model="filterActive" class="filterBox" size="small" placeholder="Search for Project")
+      PickList(
+        v-model="pickListProjects"
+        dataKey="id"
+        class="w-full"
+        @move-to-source="handleMoveToInactive"
+        @move-to-target="handleMoveToActive"
+        :showSourceControls="false"
+        :showTargetControls="false"
+        :moveAllToSourceProps="{ style: 'display: none' }"
+        :moveAllToTargetProps="{ style: 'display: none' }"
+      )
+        template(#sourceheader)
+          .text-xl.font-bold Inactive ({{ inactiveProjectCount }})
+        template(#targetheader)
+          .text-xl.font-bold Active ({{ activeProjectCount }})
+        template(#item="slotProps") {{ slotProps.item.name }}
+      ClickableButton.mt-5(title="Save Projects" @click="handleSaveProjects")
+    .stepCard
+      .cardTitle Step 3: Generate Teams
 </template>
 
 <script setup lang="ts">
-// TODO: componentize inpout & button & text, fix layout
-// TODO: needs tailwind, should be renamed to Generate.vue
-import { ref } from 'vue';
-import { useDragAndDrop } from '@formkit/drag-and-drop/vue';
+import type { Semester } from '@prisma/client';
+import { displaySemester } from '@/utils/index';
+import Dropdown from 'primevue/dropdown';
+import type { ProjectWithSemesters } from '~/server/api/projects/index.get';
+import type { PickListMoveToSourceEvent, PickListMoveToTargetEvent } from 'primevue/picklist';
+import { filterProjectsByName, getActiveProjects, getAvailableProjects } from '~/server/services/projectService';
 
 useHead({ title: 'Generate Teams' });
+const { successToast, errorToast, infoToast } = usePrimeVueToast();
 
-const [parent, options] = useDragAndDrop([
-  'Option A',
-  'Option B',
-  'Option C'
-]);
-const maxTeamSize = ref(5);
+const semesters = ref<Semester[]>([]);
+const projects = ref<ProjectWithSemesters[]>([]);
+const selectedSemester = ref<Semester | null>(null);
+
+// these store the total inactive and active projects, unfiltered by name
+const inactiveProjects = ref<ProjectWithSemesters[]>([]);
+const activeProjects = ref<ProjectWithSemesters[]>([]);
+
+// filter strings
+const filterInactive = ref<string>('');
+const filterActive = ref<string>('');
+
+// model used by Picklist.  first array corresponds to inactive projects, second to active projects.
+const pickListProjects = ref<[ProjectWithSemesters[], ProjectWithSemesters[]]>([[], []]);
+
+// arrays to store project ids we want to activate or deactivate for the selected semester
+let activateProjectsById: string[] = [];
+let deactivateProjectsById: string[] = [];
+
+onMounted(async () => {
+  semesters.value = await $fetch<Semester[]>("api/semesters");
+  projects.value = await $fetch<ProjectWithSemesters[]>("api/projects");
+});
+
+watch(selectedSemester, (newValue) => {
+  // clear filters
+  filterInactive.value = '';
+  filterActive.value = '';
+
+  // clear projects-to-be-updated arrays
+  activateProjectsById = [];
+  deactivateProjectsById = [];
+
+  inactiveProjects.value = getAvailableProjects(projects.value, newValue);
+  activeProjects.value = getActiveProjects(projects.value, newValue);
+
+  pickListProjects.value = [
+    inactiveProjects.value,
+    activeProjects.value
+  ];
+});
+
+watch(filterInactive, (newValue) => {
+  pickListProjects.value[0] = filterProjectsByName(inactiveProjects.value, newValue);
+})
+
+watch(filterActive, (newValue) => {
+  pickListProjects.value[1] = filterProjectsByName(activeProjects.value, newValue);
+})
+
+const inactiveProjectCount = computed(() => (
+  inactiveProjects.value.length
+));
+
+const activeProjectCount = computed(() => (
+  activeProjects.value.length
+));
+
+const handleMoveToInactive = (event: PickListMoveToSourceEvent) => {
+  event.items.forEach((project: ProjectWithSemesters) => {
+    // account for if we moved a project and moved it back
+    const idxInIds = activateProjectsById.indexOf(project.id);
+
+    if (idxInIds === -1) {
+      deactivateProjectsById.push(project.id);
+    } else {
+      activateProjectsById.splice(idxInIds, 1);
+    }
+
+    // also move project from activeProjects to inactiveProjects
+    // so we can track project count and filter correctly
+    activeProjects.value = activeProjects.value.filter(inactiveProject => inactiveProject.id !== project.id);
+    inactiveProjects.value.push(project);
+  })
+}
+
+const handleMoveToActive = (event: PickListMoveToTargetEvent) => {
+  event.items.forEach((project: ProjectWithSemesters) => {
+    // account for if we moved a project and moved it back
+    const idxInIds = deactivateProjectsById.indexOf(project.id);
+
+    if (idxInIds === -1) {
+      activateProjectsById.push(project.id);
+    } else {
+      deactivateProjectsById.splice(idxInIds, 1);
+    }
+
+    // also move project from inactiveProjects to activeProjects
+    // so we can track project count and filter correctly
+    inactiveProjects.value = inactiveProjects.value.filter(inactiveProject => inactiveProject.id !== project.id);
+    activeProjects.value.push(project);
+  })
+}
+
+const handleSaveProjects = async () => {
+  const semesterId = selectedSemester.value?.id;
+
+  if (!semesterId) {
+    errorToast('Please select semester and move projects before saving.');
+    return;
+  }
+
+  if (activateProjectsById.length === 0 && deactivateProjectsById.length === 0) {
+    infoToast('Select projects to move before saving.');
+    return;
+  }
+
+  if (activateProjectsById.length > 0) {
+    const response = await $fetch("/api/teams", {
+      method: "POST",
+      body: {
+        semesterId,
+        projectIds: activateProjectsById
+      }
+    })
+    
+    if (response.status === 201) {
+      activateProjectsById = [];
+      successToast(`${response.data.count} projects activated!`);
+    } else {
+      errorToast('Error activating projects.  Please try again.');
+    }
+  }
+
+  if (deactivateProjectsById.length > 0) {
+    const response = await $fetch("/api/teams", {
+      method: "DELETE",
+      body: {
+        semesterId,
+        projectIds: deactivateProjectsById
+      }
+    })
+
+    if (response.status === 200) {
+      deactivateProjectsById = [];
+      successToast(`${response.data.count} projects deactivated!`);
+    } else {
+      errorToast('Error deactivating projects.  Please try again.');
+    }
+  }
+}
 </script>
 
 <style scoped>
-ol li::marker {
-  font-size: 1.875rem;
-  color: rgb(253,245,217);
-  padding-right: 100px;
+.stepCard {
+  /* centered-col teal-card */
+  @apply flex flex-col justify-center items-center bg-teal rounded-3xl p-10 w-full
+}
+.cardTitle {
+  text-shadow: 1px 1px 1px #0000008b;
+  @apply text-3xl
+}
+.filterBox {
+  @apply w-full
 }
 </style>
