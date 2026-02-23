0eb4e1669755ae700aed305428a1f5ffb22626f9
nishthefish06 <nishanth06.us@gmail.com>
2026-02-22 21:46:55 -0600

    white gradient (will remove if needed)

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   Toast
-  .flex.flex-row.h-screen.w-full.items-start
+  .flex.flex-row.min-h-screen.w-full.items-start
     Navbar(v-if="$route.name !== 'index'")
     .centered-row.flex-auto.content-area(class="ml-0 xl:ml-64 md:border-l md:border-gray-200")
       NuxtPage
